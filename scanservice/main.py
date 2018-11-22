1af0c0b7919ea7ec8824b08e69a1114150d64b28
root <root@localhost.localdomain>
2018-11-22 12:30:24 +0800

    20181122

--- scanservice/main.py
+++ scanservice/main.py
@@ -219,10 +219,7 @@ def nmap():
         os_check = ' -O'
     else:
         os_check = ''
-    if mode == '1':
-        port_opt = ''
-    else:
-        port_opt = '-p ' + target_port
+    port_opt = '-p ' + target_port
     if len(tcp_strategy) == 0:
         command = 'nmap -Pn -sV {target} {os_check} {ports} --open -oX {filename}'
         for ip in nmap_ip:
